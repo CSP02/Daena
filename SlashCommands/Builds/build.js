25b4aaf6c340ffc0b1d1cc7220847f2f1c16070c
CSP02 <chandrapilla2002@gmail.com>
2023-11-06 11:47:01 +0530

    optimised code

--- SlashCommands/Builds/build.js
+++ SlashCommands/Builds/build.js
@@ -36,30 +36,25 @@ module.exports = {
 		let buildLevel = interaction.options.getString("level");
 		if (buildLevel == null) buildLevel = "Beginner";
 		const availableChars = fs.readdirSync(`./CharacterBuilds/${buildLevel}/`)
-		if(availableChars.filter(file => file === `${reqCharacter}.json`).length === 0) return interaction.reply("character not found!\nIf the character is newly added to the game it takes some time to get the information of best builds possible. So be patient! orrrrrrrr Submit your build!")
+		if (availableChars.filter(file => file === `${reqCharacter}.json`).length === 0) return interaction.reply("character not found!\nIf the character is newly added to the game it takes some time to get the information of best builds possible. So be patient! orrrrrrrr Submit your build!")
 		const charDetails = require(`../../CharacterBuilds/${buildLevel}/${reqCharacter.toLowerCase()}.json`);
 		const buildRole = interaction.options.getString("role")
 		const roleDetails = charDetails.Role_Build.filter(role => buildRole.split(' ')[0].includes(role.Build_Name))[0]
 		if (!roleDetails) return interaction.reply("There is no build data found for this certain build type for this character.")
 		const embedMsg = new Discord.EmbedBuilder()
-			.setColor(charDetails.Embed_Color.toString())
+			.setColor(charDetails.Embed_Color)
 			.setTitle(`${reqCharacter}'s Build`)
 			.addFields(
-				{ name: "Character Name:", value: charDetails.Character_Name.toString(), inline: true },
-				{ name: "Character Title:", value: charDetails.Character_Title.toString(), inline: true },
-				{ name: "Build type:", value: buildRole.replace("_Build", '').toString(), inline: true },
-				{ name: "Best Artifact set (4x):", value: roleDetails.Best_Artifacts_4x.toString(), inline: true },
-				{ name: "Best Artifact set (2x):", value: roleDetails.Best_Artifacts_2x.toString(), inline: true },
-				{ name: `weapons for ${buildRole.replace("_Build", '')} build`, value: roleDetails.weapon, inline: true },
+				{ name: "Character Name:", value: charDetails.Character_Name, inline: true },
+				{ name: "Character Title:", value: charDetails.Character_Title, inline: true },
+				{ name: "Build type:", value: buildRole.replace("_Build", ''), inline: true },
+				{ name: "Best Artifact set (4x):", value: roleDetails.Best_Artifacts_4x, inline: true },
+				{ name: "Best Artifact set (2x):", value: roleDetails.Best_Artifacts_2x, inline: true },
+				{ name: `weapons for ${buildRole.replace("_Build", '')} build`, value: roleDetails.weapon },
 			)
 			.setDescription(`You are viewing the ${buildLevel} build guide. To view ${buildLevel === "Beginner" ? "advanced" : "beginner"} build guide use \`\`/showbuild <character> <role> level: ${buildLevel === "Beginner" ? "Advanced" : "Beginner"}\`\` command.`)
 			.setThumbnail(charDetails.Character_Image_URL)
 			.setFooter({ text: "All the images that shown here belongs to hoyoverse.\nAll the information is based on keqingmains and some players." })
-		if (buildLevel === "Advanced") {
-			embedMsg.addFields(
-				{ name: "Team compositions:", value: "NA(going to add soon)" }
-			)
-		}
 		interaction.reply({ embeds: [embedMsg] });
 	}
 };
